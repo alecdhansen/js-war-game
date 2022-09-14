540daf738108e1f43585b23c5a9b79ad9bdba286
Alec Hansen <alecdhansen@Alecs-MacBook-Air.local>
2022-09-14 15:41:30 -0400

    began js setup

--- js/main.js
+++ js/main.js
@@ -1,2 +1,9 @@
+const playHandButton = document.querySelector(".play-hand");
+let calculation = [];
 
-
+function pushButton() {
+  playHandButton.addEventListener("click", function () {
+    alert("You clicked the Play Hand button");
+  });
+}
+pushButton();
